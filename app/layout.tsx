5c2d06f3459b47e100e5987d9b42ed61b83c8ccf
kaustubhkhare-cpu <kaustubh.khare@drmorepenhome.com>
2025-08-28 11:23:15 +0530

    Add GTM head .tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${merriweather.variable} antialiased`}>
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJD3B6R6');</script>
+<!-- End Google Tag Manager -->
+      <head>
         {/* Facebook Domain Verification */}
         <meta name="facebook-domain-verification" content="gt6aureg961lxvv5s4pkoeygxnyp9v" />
         
